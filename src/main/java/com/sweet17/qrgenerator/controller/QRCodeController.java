7c454b9c799c8230a251fa06fb1666e72321563c
irsyadputranto <irsyad.putranto@sg-edts.com>
2021-11-14 15:15:49 +0700

    Add image qr code,  add increase point, add validation

--- src/main/java/com/sweet17/qrgenerator/controller/QRCodeController.java
+++ src/main/java/com/sweet17/qrgenerator/controller/QRCodeController.java
@@ -1,12 +1,22 @@
 package com.sweet17.qrgenerator.controller;
 
+import antlr.CodeGenerator;
 import com.google.zxing.WriterException;
 import com.sweet17.qrgenerator.QrCodeGeneratorService;
+import com.sweet17.qrgenerator.dto.LinkDto;
+import com.sweet17.qrgenerator.dto.PointDto;
+import com.sweet17.qrgenerator.dto.ResponseDto;
 import com.sweet17.qrgenerator.dto.TransactionDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URI;
 
@@ -30,6 +40,24 @@ public class QRCodeController {
         return ResponseEntity.created(URI.create("/create/")).body(responseData);
     }
 
+    @PostMapping(value = "/images-qrcode", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<BufferedImage> createImage(@ModelAttribute("request") TransactionDto request, Model model) throws Exception{
+        TransactionDto responseData = qrCodeGeneratorService.savingData(request);
+        String qrCodePath = qrCodeGeneratorService.createQR(responseData);
+        model.addAttribute("code", qrCodePath);
+        return successResponse(qrCodeGeneratorService.createQRRemote(responseData));
+    }
+
+    private ResponseEntity<BufferedImage> successResponse(BufferedImage image) {
+        return new ResponseEntity<>(image, HttpStatus.OK);
+    }
+
+    @Bean
+    public HttpMessageConverter<BufferedImage> createImageHttpMessageConverter() {
+        return new BufferedImageHttpMessageConverter();
+    }
+
+
 
     @GetMapping(value = "/read-qrcode")
     public String verifyQR(@RequestParam("qrImage") String qrImage, Model model) throws Exception {
@@ -43,6 +71,13 @@ public class QRCodeController {
         TransactionDto transactionDto = qrCodeGeneratorService.get(link);
         return ResponseEntity.created(URI.create("/" + link)).body(transactionDto);
     }
+
+    @PostMapping("/{link}")
+    public ResponseEntity<ResponseDto> validationCode(@PathVariable(value = "link") LinkDto link) {
+        ResponseDto validation = qrCodeGeneratorService.validation(link);
+        return ResponseEntity.created(URI.create("/validate")).body(validation);
+    }
+
 
 }
 
---
+++ src/main/java/com/sweet17/qrgenerator/dto/LinkDto.java
@@ -0,0 +1,14 @@
+package com.sweet17.qrgenerator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LinkDto {
+    private String link;
+}
